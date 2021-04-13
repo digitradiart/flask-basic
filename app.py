912283247893fb48c61003393d59bfdfe14fa6ec
digitradiart <digitradiart@gmail.com>
2021-04-13 17:55:31 +0700

    server routing-dinamis

--- app.py
+++ app.py
@@ -10,3 +10,8 @@ def index():
 @app.route('/setting')
 def show_setting():
     return 'Ini halaman berikutnya: setting'
+
+# halaman dinamis, misal profil dari user
+@app.route('/profile/<username>')
+def show_profile(username):
+    return 'Kamu berada di halaman profile user: %s' % username
