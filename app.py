e431883c5343fffa6521d54488cc38edb6d73026
digitradiart <digitradiart@gmail.com>
2021-04-13 17:50:47 +0700

    server routing-statis

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
 
+# SERVER ROUTING
+# halaman statis
 @app.route('/')
-def hello():
+def index():
     return 'Halo lur!'
+
+@app.route('/setting')
+def show_setting():
+    return 'Ini halaman berikutnya: setting'
