e383e26834f69d3c1160cbc324b1c5720db09e30
digitradiart <digitradiart@gmail.com>
2021-04-18 06:29:15 +0700

    akses form metode post

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 # SERVER ROUTING
@@ -8,5 +8,10 @@ def index():
 
 @app.route('/profile/<username>')
 def show_profile(username):
-    # return 'Kamu berada di halaman profile user: %s' % username
     return render_template('profile.html', user=username)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return 'Email kamu adalah ' + request.form['email'] + '\nPassword kamu adalah ' + request.form['password']
+    return render_template('login.html')
